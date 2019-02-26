c0993d7001acc6dda9e6f2e716a076b0c100347e
adamConnerSax <adam_conner_sax@yahoo.com>
2019-02-25 23:59:51 -0500

    Put readable back.  Derived Parseable from that as is the typical case in Frames.

--- src/Frames/ParseableTypes.hs
+++ src/Frames/ParseableTypes.hs
@@ -6,24 +6,30 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Frames.ParseableTypes
   (
-    ColumnsWithDayAndLocalTime
+    FrameDay(..)
+  , FrameLocalTime(..)
+  , ColumnsWithDayAndLocalTime
   )
 where
 
 import           Control.Monad         (msum)
+import qualified Data.Readable         as R
 import qualified Data.Text             as T
 import qualified Data.Time.Calendar    as Time
 import qualified Data.Time.Format      as Time
 import qualified Data.Time.LocalTime   as Time
+import           Data.Typeable         (Typeable)
 import qualified Data.Vector           as V
 import qualified Frames                as F
 import qualified Frames.ColumnTypeable as F
 import qualified Frames.InCore         as F
 
-type instance F.VectorFor Time.Day = V.Vector
+newtype FrameDay = FrameDay { unFrameDay :: Time.Day } deriving (Show, Eq, Ord, Typeable)
 
-instance F.Parseable Time.Day where
-  parse t = fmap F.Definitely $
+type instance F.VectorFor FrameDay = V.Vector
+
+instance R.Readable FrameDay where
+  fromText t = fmap FrameDay $ do
     msum
       [
         Time.parseTimeM False Time.defaultTimeLocale (Time.iso8601DateFormat Nothing) (T.unpack t)
@@ -31,14 +37,21 @@ instance F.Parseable Time.Day where
       , Time.parseTimeM False Time.defaultTimeLocale "%F" (T.unpack t)
       ]
 
-type instance F.VectorFor Time.LocalTime = V.Vector
+instance F.Parseable FrameDay where
+--  parse = fmap F.Definitely . R.fromText
 
-instance F.Parseable Time.LocalTime where
-  parse t = fmap F.Definitely $
+newtype FrameLocalTime = FrameLocalTime { unFrameLocalTime :: Time.LocalTime } deriving (Show, Eq, Ord, Typeable)
+
+type instance F.VectorFor FrameLocalTime = V.Vector
+
+instance R.Readable FrameLocalTime where
+ fromText t = fmap FrameLocalTime $
     msum
       [
         Time.parseTimeM False Time.defaultTimeLocale "%c" (T.unpack t)
       ]
 
+instance F.Parseable FrameLocalTime where
+--  parse = fmap F.Definitely . R.fromText
 
-type ColumnsWithDayAndLocalTime = Time.Day ': Time.LocalTime ': F.CommonColumns
+type ColumnsWithDayAndLocalTime = FrameDay ': F.CommonColumns
