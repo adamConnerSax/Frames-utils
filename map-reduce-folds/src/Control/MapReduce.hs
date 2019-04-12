ed651b4b8058521a9987d3083b35b3536128db9e
adamConnerSax <adam_conner_sax@yahoo.com>
2019-04-12 14:34:51 -0400

    Moved map-reduce-folds to its own repo.

--- map-reduce-folds/src/Control/MapReduce.hs
+++
@@ -1,8 +0,0 @@
-module Control.MapReduce
-  ( module Control.MapReduce.Simple
-  , module Control.MapReduce.Core
-  )
-where
-import           Control.MapReduce.Core
-import           Control.MapReduce.Simple
-
