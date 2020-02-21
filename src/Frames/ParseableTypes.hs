699f6c45bb6eb1aacb70fc644a107e4132b073f9
adamConnerSax <adam_conner_sax@yahoo.com>
2020-02-21 12:33:12 -0500

    Updated for ghc-8.8.  Had to fiddle with ParseableTypes.  Ugh.

--- src/Frames/ParseableTypes.hs
+++ src/Frames/ParseableTypes.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE CPP                  #-}
 {-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE RankNTypes           #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 {-# LANGUAGE TypeApplications     #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeOperators        #-}
@@ -23,6 +26,10 @@ import qualified Data.Vector           as V
 import qualified Frames                as F
 import qualified Frames.ColumnTypeable as F
 import qualified Frames.InCore         as F
+#if __GLASGOW_HASKELL__ < 808
+--import           Control.Monad.Fail (MonadFail(fail))
+#endif  
+
 
 newtype FrameDay = FrameDay { unFrameDay :: Time.Day } deriving (Show, Eq, Ord, Typeable)
 
@@ -30,12 +37,15 @@ type instance F.VectorFor FrameDay = V.Vector
 
 instance R.Readable FrameDay where
   fromText t = fmap FrameDay $ do
-    msum
-      [
-        Time.parseTimeM False Time.defaultTimeLocale (Time.iso8601DateFormat Nothing) (T.unpack t)
-      , Time.parseTimeM False Time.defaultTimeLocale "%D" (T.unpack t)
-      , Time.parseTimeM False Time.defaultTimeLocale "%F" (T.unpack t)
-      ]
+    let parsedM = msum
+          [
+            Time.parseTime Time.defaultTimeLocale (Time.iso8601DateFormat Nothing) (T.unpack t)
+          , Time.parseTime Time.defaultTimeLocale "%D" (T.unpack t)
+          , Time.parseTime Time.defaultTimeLocale "%F" (T.unpack t)
+          ]
+    case parsedM of
+      Just x -> return x
+      Nothing -> fail (T.unpack $ "Parse Error reading \"" <> t <> "\" as Day") 
 
 instance F.Parseable FrameDay where
 --  parse = fmap F.Definitely . R.fromText
@@ -45,11 +55,14 @@ newtype FrameLocalTime = FrameLocalTime { unFrameLocalTime :: Time.LocalTime } d
 type instance F.VectorFor FrameLocalTime = V.Vector
 
 instance R.Readable FrameLocalTime where
- fromText t = fmap FrameLocalTime $
-    msum
-      [
-        Time.parseTimeM False Time.defaultTimeLocale "%c" (T.unpack t)
-      ]
+ fromText t = fmap FrameLocalTime $ do
+   let parsedM = msum
+         [
+           Time.parseTimeM False Time.defaultTimeLocale "%c" (T.unpack t)
+         ]
+   case parsedM of
+     Just x -> return x
+     Nothing -> fail (T.unpack $ "Parse Error reading \"" <> t <> "\" as LocalTime") 
 
 instance F.Parseable FrameLocalTime where
 --  parse = fmap F.Definitely . R.fromText
