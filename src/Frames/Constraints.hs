8020304966550ec5b2a4bc525417e5bf8b21248d
adamConnerSax <adam_conner_sax@yahoo.com>
2020-02-28 12:58:01 -0500

    Bummping upper bounds

--- src/Frames/Constraints.hs
+++ src/Frames/Constraints.hs
@@ -1,24 +1,25 @@
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+{-# LANGUAGE ConstraintKinds       #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeApplications      #-}
-{-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE RankNTypes            #-}
-{-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE ConstraintKinds       #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE UndecidableInstances  #-}
-{-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Frames.Constraints
   ( EachIs
+  , LeftJoinC
   )
 where
 
@@ -28,11 +29,29 @@ import qualified Data.Vinyl.XRec               as V
 import           Frames                         ( (:.) )
 import qualified Frames                        as F
 import qualified Frames.Melt                   as F
+import qualified Frames.InCore                 as FI
 
 import           Data.Kind                      ( Constraint )
+import           Data.Discrimination            ( Grouping)
 
 -- | Type family to create constraints that apply a constraint taking a type-list
 -- and a single type and apply it to all types in another list
 type family EachIs (c :: [k] -> k -> Constraint) (rs :: [k]) (ts :: [k]) :: Constraint where
   EachIs _ _ '[] = ()
   EachIs c rs (t ': ts) = (c rs t, EachIs c rs ts)
+
+
+-- leftJoin @fs (F.FrameRec @rs) (F.FrameRec @rs2) :: F.FrameRec @rs2'
+type LeftJoinC fs rs rs2 rs2' = (fs F.⊆ rs
+                                , fs F.⊆ rs2
+                                , rs F.⊆ (rs V.++ rs2')
+                                , rs2' F.⊆ rs2
+                                , rs2' ~ F.RDeleteAll fs rs2
+                                , V.RMap rs
+                                , V.RMap (rs V.++ rs2')
+                                , V.RecApplicative rs2'
+                                , Grouping (F.Record fs)
+                                , FI.RecVec rs
+                                , FI.RecVec rs2'
+                                , FI.RecVec (rs V.++ rs2')
+                                )
