c560b957b70ba0b6e5bbabcfde75069efe186b88
adamConnerSax <adam_conner_sax@yahoo.com>
2020-04-16 16:01:00 -0400

    Adding some bulk transform stuff

--- src/Frames/Transform.hs
+++ src/Frames/Transform.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DerivingVia         #-}
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE PolyKinds           #-}
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE DerivingVia         #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Frames.Transform
@@ -21,8 +23,14 @@ module Frames.Transform
   , recordSingleton
   , addColumn
   , dropColumn
+  , replaceColumn
   , dropColumns
   , retypeColumn
+  , ReplacerList(..)
+  , transformRL
+  , rlRename
+  , rlTransform
+  , RetypeColumns(..)
   , reshapeRowSimple
   , reshapeRowSimpleOnOne
   , FieldDefaults (..)
@@ -37,6 +45,7 @@ module Frames.Transform
 
 --import qualified Control.Newtype as N
 import           Control.Applicative   ((<|>))
+import           Data.Kind (Type)
 import qualified Data.Text            as T
 import qualified Data.Vinyl           as V
 import           Data.Vinyl.TypeLevel as V --(type (++), Snd)
@@ -74,6 +83,62 @@ mutate f xs = xs `F.rappend` f xs
 recordSingleton :: forall af s a. (KnownSymbol s, af ~ '(s,a)) => a -> F.Record '[af]
 recordSingleton a = a F.&: V.RNil
 
+
+replaceColumn :: forall t t' rs. (V.KnownField t
+                                 , V.KnownField t'
+                                 , (RDelete t rs V.++ '[t']) F.⊆ (rs V.++ '[t'])
+                                 , ElemOf rs t
+                                 )
+              => (V.Snd t -> V.Snd t')
+              -> F.Record rs -> F.Record (RDelete t rs V.++ '[t'])
+replaceColumn f = F.rcast @(RDelete t rs V.++ '[t']) . mutate (recordSingleton @t' . f . F.rgetField @t)
+
+
+appendReplacement :: forall t t' as bs. (V.KnownField t, V.KnownField t')
+                  => (F.Record as -> F.Record bs) -> (V.Snd t -> V.Snd t') -> F.Record (t ': as) -> (F.Record (t' ': bs))
+appendReplacement replaceTail replaceHead (t V.:& xs) = (replaceHead $ V.getField t) F.&: (replaceTail xs)
+
+doNothingToNothing :: F.Record '[] -> F.Record '[]
+doNothingToNothing = id
+
+-- appendReplacement (appendReplacement (appendReplacement doNothingToNothing replaceA) replaceB) replaceC
+-- ((doNothingToNothing `appendReplacement` replaceA) `appendReplacement` replaceB) `appendReplacement` replaceC
+
+newtype ColReplacer t t' = ColReplacer { unColReplacer :: V.Snd t -> V.Snd t' }
+
+data ReplacerList (as :: [(Symbol, Type)]) (bs :: [(Symbol, Type)]) where
+  RLNil :: ReplacerList '[] '[]
+  RLCons :: (V.KnownField t, V.KnownField t') => ColReplacer t t' -> ReplacerList as bs -> ReplacerList (t ': as) (t' ': bs)
+
+rlRename :: forall t t'. (V.KnownField t, V.KnownField t', V.Snd t ~ V.Snd t') => ColReplacer t t'
+rlRename = ColReplacer id
+
+rlTransform :: forall t t'. (V.KnownField t, V.KnownField t', V.Snd t ~ V.Snd t') => (V.Snd t -> V.Snd t') -> ColReplacer t t'
+rlTransform f = ColReplacer f
+
+
+
+buildReplacer :: ReplacerList as bs -> F.Record as -> F.Record bs
+buildReplacer RLNil = doNothingToNothing
+buildReplacer (rh `RLCons` rl) = appendReplacement (buildReplacer rl) (unColReplacer rh)
+
+
+-- buildReplacer (f1 `rlTransform` @a @b` f2 `rlTransform @x @y` RLNil)   
+
+
+transformRL :: (as F.⊆ rs, RDeleteAll as rs F.⊆ rs)
+            => ReplacerList as bs -> F.Record rs -> F.Record (RDeleteAll as rs V.++ bs)
+transformRL rl = transform (buildReplacer rl)
+
+
+
+{-
+data RowTransform (as :: [Symbol, Type]) (bs :: [Symbol, Type]) (x :: Type) where
+  ChangeColValue :: (V.KnownField t, ElemOf as t) => (V.Snd t -> V.Snd t) -> RowTransform as as x
+  ChangeColName :: (V.KnownField t, KnownSymbol s, ElemOf as t) => RowTransform as ((RDelete t as V.++ '[ '(s, V.Snd t)]))
+-}
+  
+
 -- | add a column
 addColumn :: forall t bs. (V.KnownField t) => V.Snd t -> F.Record bs -> F.Record (t ': bs)
 addColumn x r = recordSingleton x `V.rappend` r
@@ -96,7 +161,54 @@ retypeColumn :: forall x y rs. ( V.KnownField x
 retypeColumn = transform @rs @'[x] @'[y] (\r -> (F.rgetField @x r F.&: V.RNil))
 
 
+-- take a type-level-list of (fromName, toName, type) and use it to rename columns in suitably typed record
+type family FromRecList (a :: [(Symbol, Symbol, Type)]) :: [(Symbol, Type)] where
+  FromRecList '[] = '[]
+  FromRecList ('(fs, ts, t) ': rs) = '(fs, t) ': FromRecList rs
 
+type family ToRecList (a :: [(Symbol, Symbol, Type)]) :: [(Symbol, Type)] where
+  ToRecList '[] = '[]
+  ToRecList ('(fs, ts, t) ': rs) = '(ts, t) ': ToRecList rs  
+  
+class (FromRecList cs F.⊆ rs) => RetypeColumns (cs :: [(Symbol, Symbol, Type)]) (rs :: [(Symbol, Type)]) where
+  retypeColumns :: (rs ~ (rs V.++ '[])) => F.Record rs -> F.Record (RDeleteAll (FromRecList cs) rs V.++ (ToRecList cs))
+
+instance RetypeColumns '[] rs where
+  retypeColumns = id
+
+instance (RetypeColumns cs rs
+         , V.KnownField '(fs, t)
+         , V.KnownField '(ts, t)
+         , ElemOf rs '(fs, t)
+         , (RDelete '(fs, t) (RDeleteAll (FromRecList cs) rs V.++ ToRecList cs) V.++ '[ '(ts, t)])
+         ~ (RDeleteAll (FromRecList cs) (RDelete '(fs, t) rs) V.++ ('(ts, t) ': ToRecList cs))
+         , ElemOf (RDeleteAll (FromRecList cs) rs ++ ToRecList cs) '(fs, t)
+         , (RDelete '(fs, t) (RDeleteAll (FromRecList cs) rs ++ ToRecList cs)) F.⊆ (RDeleteAll (FromRecList cs) rs ++ ToRecList cs)
+         )
+    => RetypeColumns ('(fs, ts, t) ': cs) rs where
+  retypeColumns = retypeColumn @'(fs, t) @'(ts, t) @(RDeleteAll (FromRecList cs) rs V.++ (ToRecList cs))  . retypeColumns @cs @rs
+
+{-
+-- take a type-level-list of (fromName, toName, type -> type) and use it to transform columns in suitably typed record
+type family FromTList (a :: [(Symbol, Symbol, Type -> Type)]) :: [(Symbol, Type)] where
+  FromRecList '[] = '[]
+  FromRecList ('(fs, ts, x -> y) ': rs) = '(fs, x) ': FromRecList rs
+
+type family ToTList (a :: [(Symbol, Symbol, Type -> Type)]) :: [(Symbol, Type)] where
+  ToRecList '[] = '[]
+  ToRecList ('(fs, ts, x -> y) ': rs) = '(ts, y) ': ToRecList rs  
+  
+class (FromRecList cs F.⊆ rs) => RetypeColumns (cs :: [(Symbol, Symbol, Type -> Type)]) (rs :: [(Symbol, Type)]) where
+  retypeColumns :: (rs ~ (rs V.++ '[])) => F.Record rs -> F.Record (RDeleteAll (FromRecList cs) rs V.++ (ToRecList cs))
+
+instance TransformColumns '[] rs where
+  retypeColumns = id
+
+instance (RetypeColumns cs rs
+         )
+    => RetypeColumns ('(fs, ts, t) ': cs) rs where
+  retypeColumns = retypeColumn @'(fs, t) @'(ts, t) @(RDeleteAll (FromRecList cs) rs V.++ (ToRecList cs))  . retypeColumns @cs @rs
+-}
 
 -- This is an anamorphic step.
 -- You could also use meltRow here.  That is also (Record as -> [Record bs])
