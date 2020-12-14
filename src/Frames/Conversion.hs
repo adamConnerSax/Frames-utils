8100599ca2fcd66ab41c2747f401736ee3a8957f
adamConnerSax <adam_conner_sax@yahoo.com>
2020-12-14 16:33:41 -0500

    Added type-safe row conversions to single type helpers

--- src/Frames/Conversion.hs
+++ src/Frames/Conversion.hs
@@ -1,16 +1,24 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+--{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs #-}
+--{-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeOperators       #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Frames.Conversion
   (
     toList
+  , toListVia
+  , mkConverter
   ) where
 
 import qualified Data.Vinyl           as V
 import           Data.Vinyl.Functor   (Identity(..), Const(..))
+import qualified Data.Vinyl.Functor as V
+import qualified Data.Vinyl.TypeLevel as V
 import qualified Frames               as F
 
 
@@ -22,6 +30,14 @@ toList :: forall a as rs. (V.RecordToList (V.Unlabeled as)
   => F.Rec F.ElField rs -> [a] --(F.RecordToList as, ) => F.Record F.ElField as -> [a]
 toList = V.recordToList . V.rmapMethod @((~) a) (Const . getIdentity) . V.stripNames . F.rcast @as
 
+
+type ToTypeRecF a = V.Lift (->) V.ElField (V.Const a)
+
+toListVia :: (V.RecordToList rs, V.RApply rs) => V.Rec (ToTypeRecF a) rs -> F.Record rs -> [a]
+toListVia converters = V.recordToList . V.rapply converters
+
+mkConverter :: (V.KnownField t, V.Snd t ~ b) => (b -> a) -> ToTypeRecF a t
+mkConverter f = V.Lift $ V.Const . f . V.getField 
 
 {-
 toMap :: forall a as rs. (V.RecordToList (V.Unlabeled as)
