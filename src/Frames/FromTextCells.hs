83f3ab886eacb1fb0d874702cab6ea3ab1965136
adamConnerSax <adam_conner_sax@yahoo.com>
2021-04-27 21:36:28 -0400

    Changed FromTextCells to use IO

--- src/Frames/FromTextCells.hs
+++ src/Frames/FromTextCells.hs
@@ -25,19 +25,19 @@ module Frames.FromTextCells
   )
 where
 
-import qualified Control.Monad.ST as ST
+--import qualified Control.Monad.ST as ST
 
 import qualified Data.Text as T
 import qualified Data.Vinyl                    as V
 import qualified Data.Vinyl.Functor            as V
-import qualified Data.Functor.Identity as I
 import qualified Frames                        as F
 import qualified Frames.CSV                             as F
 import qualified Frames.Streamly.CSV as FS
 import qualified Frames.Streamly.InCore as FS
 import qualified Streamly.Prelude as Streamly
-import qualified Streamly.Internal.Prelude as Streamly
 import qualified Control.Monad
+import Control.Exception (throwIO)
+import System.IO.Error (userError)
 
 {-
 Simplify building a frame from [[Text]], coming, e.g., from a parser,
@@ -45,15 +45,18 @@ rather than using the built-in TH machinery.
 Builds a stream of lines of text and passes those to the Frames machinery.  Then uses
 the ST monad to do the mutable bits.
 -}
-fromTextCells :: (V.RMap rs, F.ReadRec rs, FS.RecVec rs) => [[T.Text]] -> Either T.Text (F.FrameRec rs)
+fromTextCells :: (V.RMap rs, F.ReadRec rs, FS.RecVec rs) => [[T.Text]] -> IO (F.FrameRec rs)
 fromTextCells parsed = do
     let lineStream = Streamly.fromList $ fmap (T.intercalate ",") parsed -- (IsStream t, Monad m) => t m [Text]
         recEStream = FS.streamTableEither lineStream -- t m (F.Rec (Either T.Text .: ElField) X)
-        recES = sequence $ Streamly.map (F.rtraverse V.getCompose) recEStream
+        throwLeft = either (throwIO .  userError . toString) return
+        recES = Streamly.mapM (throwLeft . F.rtraverse V.getCompose) recEStream
+    FS.inCoreAoS recES
+{-
     case recES of
-      Left a -> Left a
-      Right s ->  Right $  ST.runST $ FS.inCoreAoS $ Streamly.hoist (return . I.runIdentity) s
-
+      Left a -> throwIO a
+      Right s ->  FS.inCoreAoS s
+-}
 {-
 Simplify building a frame from [[Text]], coming, e.g., from a parser,
 rather than using the built-in TH machinery.
@@ -65,26 +68,33 @@ This version parses as rs, allows modification while still a stream and then ret
 fromTextCellsMapped :: (V.RMap rs, F.ReadRec rs, FS.RecVec rs')
                     => (F.Record rs -> F.Record rs')
                     -> [[T.Text]]
-                    -> Either T.Text (F.FrameRec rs')
+                    -> IO (F.FrameRec rs')
 fromTextCellsMapped recMap parsed = do
     let lineStream = Streamly.fromList $ fmap (T.intercalate ",") parsed -- (IsStream t, Monad m) => t m [Text]
         recEStream = FS.streamTableEither lineStream -- t m (F.Rec (Either T.Text .: ElField) X)
-        recES = sequence $ Streamly.map (F.rtraverse V.getCompose) recEStream
-    case recES of
-      Left a -> Left a
-      Right s ->  Right $  ST.runST $ FS.inCoreAoS $ Streamly.hoist (return . I.runIdentity) $ Streamly.map recMap s
+        throwLeft = either (throwIO .  userError . toString) return
+        recES = Streamly.mapM (throwLeft . F.rtraverse V.getCompose) recEStream
+    FS.inCoreAoS $ Streamly.map recMap recES
+{-
 
+    case recES of
+      Left a -> throwIO a
+      Right s ->  FS.inCoreAoS $ Streamly.map recMap s
+-}
 
 
 fromTextCellsMappedE :: (V.RMap rs, F.ReadRec rs, FS.RecVec rs')
                     => (F.Record rs -> Either T.Text (F.Record rs'))
                     -> [[T.Text]]
-                    -> Either T.Text (F.FrameRec rs')
+                    -> IO (F.FrameRec rs')
 fromTextCellsMappedE recMapE parsed = do
     let lineStream = Streamly.fromList $ fmap (T.intercalate ",") parsed -- (IsStream t, Monad m) => t m [Text]
         recEStream = FS.streamTableEither lineStream -- t m (F.Rec (Either T.Text .: ElField) X)
-        recES = sequence $ Streamly.map (recMapE Control.Monad.<=< F.rtraverse V.getCompose) recEStream
+        throwLeft = either (throwIO .  userError . toString) return
+        recES = Streamly.mapM (throwLeft . (recMapE Control.Monad.<=< F.rtraverse V.getCompose)) recEStream
+    FS.inCoreAoS recES
+{-
     case recES of
-      Left a -> Left a
-      Right s ->  Right $  ST.runST $ FS.inCoreAoS $ Streamly.hoist (return . I.runIdentity) s
-
+      Left a -> throwIO a
+      Right s ->  FS.inCoreAoS s
+-}
