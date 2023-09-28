f56631ab7669b653ffe1652666e8dbf883fdeeec
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2023-09-28 09:18:19 -0400

    Added ElemsOf

--- src/Frames/Constraints.hs
+++ src/Frames/Constraints.hs
@@ -19,17 +19,17 @@
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Frames.Constraints
   ( EachIs
-  , LeftJoinC
+  , ElemsOf
   )
 where
 
-import qualified Data.Vinyl                    as V
-import qualified Data.Vinyl.TypeLevel          as V
-import qualified Frames                        as F
+--import qualified Data.Vinyl                    as V
+--import qualified Data.Vinyl.TypeLevel          as V
+--import qualified Frames                        as F
 import qualified Frames.Melt                   as F
-import qualified Frames.InCore                 as FI
+--import qualified Frames.InCore                 as FI
 
-import           Data.Discrimination            ( Grouping)
+--import           Data.Discrimination            ( Grouping)
 
 -- | Type family to create constraints that apply a constraint taking a type-list
 -- and a single type and apply it to all types in another list
@@ -38,17 +38,6 @@ type family EachIs (c :: [k] -> k -> Constraint) (rs :: [k]) (ts :: [k]) :: Cons
   EachIs c rs (t ': ts) = (c rs t, EachIs c rs ts)
 
 
--- leftJoin @fs (F.FrameRec @rs) (F.FrameRec @rs2) :: F.FrameRec @rs2'
-type LeftJoinC fs rs rs2 rs2' = (fs F.⊆ rs
-                                , fs F.⊆ rs2
-                                , rs F.⊆ (rs V.++ rs2')
-                                , rs2' F.⊆ rs2
-                                , rs2' ~ F.RDeleteAll fs rs2
-                                , V.RMap rs
-                                , V.RMap (rs V.++ rs2')
-                                , V.RecApplicative rs2'
-                                , Grouping (F.Record fs)
-                                , FI.RecVec rs
-                                , FI.RecVec rs2'
-                                , FI.RecVec (rs V.++ rs2')
-                                )
+type family ElemsOf (rs :: [k]) (es :: [k]) :: Constraint where
+  ElemsOf _ '[] = ()
+  ElemsOf rs (t ': ts) = (F.ElemOf rs t, ElemsOf rs ts)
